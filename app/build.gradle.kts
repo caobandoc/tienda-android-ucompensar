01501c93ca40ca5d5c43f51d533e7e25a9115c11
caobandoc <obandocarlosandres@gmail.com>
2025-04-13 18:37:35 -0500

    update libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,6 @@ android {
     }
 }
 
-val glideVersion = "4.15.1"
-
 dependencies {
 
     implementation(libs.androidx.core.ktx)
@@ -56,14 +54,18 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.glide)
+
+    kapt(libs.glide)
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
-    implementation("com.github.bumptech.glide:glide:$glideVersion")
-    kapt("com.github.bumptech.glide:compiler":$glideVersion")
 }
