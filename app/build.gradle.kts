43661e0d394ea5a51ae7e9851cd580bd2351ba4d
caobandoc <obandocarlosandres@gmail.com>
2025-05-17 10:35:45 -0500

    Login old version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,8 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.glide)
 
+    implementation(libs.google.play.services.auth)
+
     kapt(libs.glide)
 
     testImplementation(libs.junit)
--- app/src/main/java/com/example/compensarshop/ui/auth/login/LoginActivity.kt
+++ app/src/main/java/com/example/compensarshop/ui/auth/login/LoginActivity.kt
@@ -2,30 +2,90 @@ package com.example.compensarshop.ui.auth.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.compensarshop.R
 import com.example.compensarshop.ui.app.products.ProductListActivity
 import com.example.compensarshop.ui.auth.register.RegisterActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 
 class LoginActivity : AppCompatActivity(){
+
+    private lateinit var btnLogin: Button
+    private lateinit var btnRegister: Button
+    private lateinit var btnGoogleSignIn: SignInButton
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private val RC_SIGN_IN = 123
+    private val TAG = "GoogleSignIn"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val loginButton: Button = findViewById(R.id.btn_login)
-        val registerButton: Button = findViewById(R.id.btn_register)
+        btnLogin = findViewById(R.id.btn_login)
+        btnRegister = findViewById(R.id.btn_register)
+        btnGoogleSignIn = findViewById(R.id.btn_google_sign_in)
 
-        loginButton.setOnClickListener {
-            val intent = Intent(this, ProductListActivity::class.java)
-            startActivity(intent)
+        //Configurar Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+
+        btnLogin.setOnClickListener {
+            navigateToProductList()
         }
 
-        registerButton.setOnClickListener {
+        btnRegister.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
 
+        btnGoogleSignIn.setOnClickListener {
+            signIn()
+        }
+
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            // Inicio de sesión exitoso, redirigir a la pantalla principal
+            navigateToProductList()
+        } catch (e: ApiException) {
+            // Error en el inicio de sesión
+            Log.w(TAG, "signInResult:failed code=" + e.statusCode)
+            Toast.makeText(this, "Error en el inicio de sesión con Google", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun navigateToProductList() {
+        val intent = Intent(this, ProductListActivity::class.java)
+        startActivity(intent)
     }
 
 }
